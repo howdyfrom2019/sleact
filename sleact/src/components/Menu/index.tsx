ca783ff31eb42eadb38af70cd59bc8491f49937f
Russel <tjd1emd@naver.com>
2022-08-01 20:35:23 +0900

    data setting with useEffect. create menus.

---
+++ sleact/src/components/Menu/index.tsx
@@ -0,0 +1,28 @@
+import { CreateMenu, CloseModalButton } from './styles';
+import React, { CSSProperties, FC, PropsWithChildren, useCallback} from 'react';
+
+interface Props {
+  show: boolean;
+  onCloseModal: () => void;
+  style?: CSSProperties;
+  closeButton?: boolean;
+}
+
+const Menu: FC<PropsWithChildren<Props>> = ({ children, style, show, onCloseModal, closeButton }) => {
+  const stopPropagation = useCallback((e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+  }, [])
+  return (
+    <CreateMenu onClick={onCloseModal}>
+      <div style={style} onClick={stopPropagation}>
+        {closeButton && <CloseModalButton onClick={onCloseModal}>&times;</CloseModalButton>}
+        {children}
+      </div>
+    </CreateMenu>
+  );
+};
+Menu.defaultProps = {
+  closeButton: true,
+};
+
+export default Menu;
--- sleact/src/layouts/Worspace/Workspace.tsx
+++ sleact/src/layouts/Worspace/Workspace.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import useSWR from "swr";
 import fetcher from "../../utils/fetcher";
 import axios from "axios";
@@ -7,10 +7,12 @@ import gravatar from 'gravatar';
 import { RightMenu, Header, Chats, Channels, MenuScroll, LogOutButton, AddButton, WorkspaceButton, WorkspaceModal, WorkspaceName, Workspaces, WorkspaceWrapper, ProfileModal, ProfileImg} from "./styles";
 import Channel from "../../pages/Channel";
 import DirectMessage from "../../pages/DirectMessage";
+import Menu from "../../components/Menu";
 
 const Workspace = () => {
     const { params } = useParams();
     let navigate = useNavigate();
+    const [showUserMenu, setShowUserMenu] = useState(false);
     const { data, error, mutate } = useSWR('http://localhost:3095/api/users', fetcher);
     const onLogout = useCallback(() => {
         axios.post('http://localhost:3095/api/users/logout', null, {withCredentials: true})
@@ -19,15 +21,30 @@ const Workspace = () => {
             });
     }, []);
 
-    if (!data) {
-        navigate('/login', {replace: true});
-    }
+    const onClickUserProfile = useCallback(() => {
+      setShowUserMenu((prev) => !prev);
+    }, []);
+
+    useEffect(() => {
+      if (!data || data === "ok") navigate('/login', {replace: true});
+    }, [data]);
+
     return (
         <div>
           <Header>
             <RightMenu>
-              <span>
+              <span onClick={onClickUserProfile}>
                 <ProfileImg src={gravatar.url(data?.nickname, {s: '28px', d: 'retro'})} alt={data?.nickname}/>
+                {showUserMenu && <Menu style={{right: 0, top: 38}} show={showUserMenu} onCloseModal={onClickUserProfile}>
+                    <ProfileModal>
+                        <img src={gravatar.url(data.nickname, {s: '36px', d: 'retro'})} alt={data.nickname} />
+                        <div>
+                            <span id={'profile-name'}>{data.nickname}</span>
+                            <span id={"profile-active"}>Active</span>
+                        </div>
+                    </ProfileModal>
+                    <LogOutButton onClick={onLogout}>로그아웃</LogOutButton>
+                </Menu>}
               </span>
             </RightMenu>
           </Header>
